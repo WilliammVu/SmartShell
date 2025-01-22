058ee0d706744e79cdd0fa9e32376836f282a796
WilliammVu <152057598+WilliammVu@users.noreply.github.com>
2025-01-22 14:44:26 -0800

    Formulate blueprint

--- file_organizer.cpp
+++ file_organizer.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <fstream>
 #include <filesystem>
-#include <stack>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-class File{
-public:
-    std::string name;
-    File* parent;
-    std::vector<File*> subFiles;
-    std::string contents;
-};
+namespace fs = std::filesystem;
 
-class FileOrganizer{
-    File* Documents; //root node
-};
+void displayHelp() {
+    std::cout << "Available commands:\n"
+              << "  ls                 List contents of the current directory\n"
+              << "  cd <path>          Change the current directory to <path>\n"
+              << "  mkdir <dir_name>   Create a directory named <dir_name>\n"
+              << "  touch <file_name>  Create an empty file named <file_name>\n"
+              << "  rm <name>          Remove the file or directory named <name>\n"
+              << "  rename <old> <new> Rename (or move) <old> to <new>\n"
+              << "  help               Show this help message\n"
+              << "  exit               Exit the program\n";
+}
